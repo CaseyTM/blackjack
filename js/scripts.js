95ef4ac9b196e5965e6436748cf01f8e81a461f8
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2016-12-14 16:55:57 -0500

    added win-logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,176 @@
-// Insta DOM load
+// -----------------GLOBALS ---------------------------------
+// -----------------GLOBALS ---------------------------------
+// -----------------GLOBALS ---------------------------------
+var theDeck = createDeck();
+var playersHand = [];     //SAME AS PLAYER1SQUARES IN TICTACTOE
+var dealersHand = []; 	  //SAME AS PLAYER2SQUARES IN TICTACTOE
+var topOfDeck = 4;
+
 $(document).ready(function(){
-		// GLOBALS
-var theDeck = [];
-var playersHand = [];
-var dealersHand = [];
-	createDeck();
+	$('.reset-button').click(function(){
+		reset();
+	});
 
-		$('.deal-button').click(function(){
-			// console.log(this);
-		
-			shuffleDeck();
-			playersHand.push(theDeck[0]);
-			dealersHand.push(theDeck[1]);
-			playersHand.push(theDeck[2]);
-			dealersHand.push(theDeck[3]);
-			// put the first card in the players hand 
-			placeCard(playersHand[0], 'player', 'one');
-			placeCard(playersHand[1], 'player', 'two');
-			placeCard(dealersHand[0], 'dealer', 'one');
-			placeCard(dealersHand[1], 'dealer', 'two');
+	$('.deal-button').click(function(){
+		// DEAL STUFF GOES IN HERE 
+		shuffleDeck();  
+		playersHand.push(theDeck.shift());
+		// theDeck.shift(); // REMOVE THE TOP OF THE DECK
+		playersHand.push(theDeck.shift());
+		// theDeck.shift();
 
-			calculateTotal('player', playersHand);
-			calculateTotal('dealer', dealersHand);
+		placeCard('player','1',playersHand[0]);
+		placeCard('player','2',playersHand[1]);
 
-		});
+		dealersHand.push(theDeck.shift());
+		// theDeck.shift();
+		dealersHand.push(theDeck.shift());
+		// theDeck.shift();
 
+		placeCard('dealer','1',dealersHand[0]);
+		placeCard('dealer','2',dealersHand[1]);
 
-		$('.hit-button').click(function(){
-			
-		});
+		calculateTotal(dealersHand, 'dealer')
+		calculateTotal(playersHand, 'player')
+
+	});
 
 
+	$('.hit-button').click(function(){
+		// HIT STUFF GOES IN HERE
+		// var whatThePlayerHas = Number($'.player.total-number').text
 
-
-
-
-		$('.stand-button').click(function(){
-				// console.log(this);
-		});
-
-	function createDeck(){
-		// fill the deck with 52 cards, 4 suits H,S,D,C
-		// 1-13 (11= J 12 = Q, 13 = K)
-		var suits = ['h','s','d','c'];
-		// LOOP THROUGH ALL 4 SUITS 
-		for(let s = 0 ; s < suits.length; s++){
-			// LOOP THROUGH ALL 13 CARDS FOR EACH SUITS
-			for(let c = 1; c <= 13; c++){
-				theDeck.push(c+suits[s]);
-
-			}
+		if(calculateTotal(playersHand, 'player') < 21){
+		playersHand.push(theDeck.shift());
+		var slotForNewCard = playersHand.length;
+		// if(playersHand.length = 3){
+		// 	slotForNewCard = "three";
+		// }else if(playersHand.length === 4){
+		// 	slotForNewCard = "four";	
+		// }else if(playersHand.length === 5){
+		// 	slotForNewCard = "five";	
+		// }else if(playersHand.length === 6){
+		// 	slotForNewCard = "six";	
+		// }
+		var lastCardIndex = playersHand.length-1
+		placeCard('player',slotForNewCard,playersHand[lastCardIndex]);
+		calculateTotal(playersHand, 'player');
 		}
-	}
+	});
 
-
-	function shuffleDeck(){
-		
-		for(let i = 0; i < 10000; i ++){
-			var card1toSwitch = Math.floor(Math.random()* theDeck.length);
-			var card2toSwitch = Math.floor(Math.random()* theDeck.length);
-			var temp = theDeck[card1toSwitch];
-			theDeck[card1toSwitch] = theDeck[card2toSwitch];
-			theDeck[card2toSwitch] = temp;
-
+	$('.stand-button').click(function(){
+		dealerTotal = calculateTotal(dealersHand, 'dealer');
+		while(dealerTotal < 17){
+			dealersHand.push(theDeck.shift());
+			var lastCardIndex = dealersHand.length-1;
+			var slotForNewCard = dealersHand.length;
+			placeCard('dealer',slotForNewCard,dealersHand[lastCardIndex]);
+			dealerTotal = calculateTotal(dealersHand, 'dealer');
 		}
-		// console.log(theDeck)
-	}
-	function placeCard(whatCard, who, whichSlot){
-		var classToTarget = '.' +who+ '-cards .card-' +whichSlot;
-		// console.log(classToTarget)
-		$(classToTarget).html('<img src="images/'+whatCard+'.png">');
-	}
-
-	function calculateTotal(who, theirHand){
-		var cardValue = 0;
-		var total = 0;
-		for(let i = 0 ; i < theirHand.length; i ++){
-			cardValue = Number(theirHand[i].slice(0, -1));
-			console.log(cardValue);
-			total += cardValue;
-
-		}
-		var classToTarget = '.'+who+'-total-number';
-		$(classToTarget).text(total);
-	}
+		checkWin();
+	});
 
 
 
 });
 
+function shuffleDeck(){
+	for(let i = 0 ; i < 9999 ; i++){
+	var random1 = Math.floor(Math.random()* theDeck.length);
+	var random2 = Math.floor(Math.random()* theDeck.length);
+	var temp = theDeck[random1];
+	theDeck[random1] = theDeck[random2];
+	theDeck[random2] = temp;
+	}};
+
+function checkWin(){
+	dealerTotal = calculateTotal(dealersHand, 'dealer');
+	playerTotal = calculateTotal(playersHand, 'player');
+	if(playerTotal > 21){
+		$('.dealer-total-number').text("Dealer won!");
+
+	}else if(dealerTotal > 21){
+		$('.dealer-total-number').text("Dealer Busted!");
+		// player is safe, dealer has busted update dom
+	}else{
+		if(playerTotal > dealerTotal){
+			$('.player-total-number').text("You won!");
+			// Player won, update the DOM
+		}else if(dealerTotal > playerTotal){
+			$('.dealer-total-number').text("Dealer won!");
+			// dealer won, update the dom
+		}else{
+			$('.player-total-number').text("Tie!");
+			$('.dealer-total-number').text("Tie!");
+			// TIE, update the DOM
+		}
+	}
+}
+
+function reset(){
+	// deck needs to be reset, player and dealer hands too 
+	// RESET THE DOM INCLUDING TOTALS
+	theDeck = createDeck();
+	playersHand = [];
+	dealersHand = [];
+	$('.card').html('');
+	playerTotal = calculateTotal(dealersHand, 'dealer');
+	dealerTotal = calculateTotal(playersHand, 'player');
 
 
+}
 
 
+function createDeck(){
+	var newDeck = [];
+	var suits = ['h', 's', 'd', 'c'];
+	for( let s = 0 ; s < suits.length; s++){
+		// suits outer loop
+		for(let c = 1; c <= 13; c++){
+			// card values inner loop
+			newDeck.push(c+suits[s]);
+		}
+	}
+	return newDeck;
+}
+function placeCard(who,where,whatCard){
+	var classSelector = '.' + who + '-cards .card-' + where;
+	$(classSelector).html('<img src="images/' + whatCard + '.png">');
 
+}
 
+function calculateTotal(hand, who){
+	var countAce = 0;
+	var total = 0; 
+	var cardValue = 0;
+	for(let i = 0 ; i < hand.length ; i ++){
+		// HANDLE THE FACE-CARDS
+	cardValue = Number(hand[i].slice(0,-1));
+		if(cardValue > 10){
+			cardValue = 10;
+		}if((cardValue === 1) && (total <11)){
+			cardValue = 11;
+			countAce++;
+		}
+		total += cardValue;
+		if((countAce > 0) && (total > 21)){
+			total -= 10;
+			countAce--;
 
+		}
+
+		
+		
+		
+	}
+	// 	// cardValue = Number(hand[i].slice(0,-1));
+	// 	total += cardValue;
+	// }
+	// update the DOM with the new total 
+	var classSelector = '.'+who+'-total-number';
+	$(classSelector).text(total);
+	return total;
+}
 
 
 
